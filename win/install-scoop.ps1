f92b4534a0b1f60c70b7a446bb234f904ec3f9b8
Vojtech Chvojka <vojtech.chvojka@rockwellautomation.com>
2023-12-04 20:22:07 +0100

    Add forensics.

---
+++ win/install-scoop.ps1
@@ -0,0 +1,3 @@
+# This downloads and executes a script that downloads and installs scoop.
+# If you are not able (or willing) to do that, you can install it your prefered way https://scoop.sh/.
+irm get.scoop.sh | iex
