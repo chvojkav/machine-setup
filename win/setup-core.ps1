b9078659c01777d44fcd3ffa4667b01040d1b217
Vojtech <vchvojka@onippoint.com>
2024-11-21 09:31:56 +0100

    Add vscode, spotify, docker and wsl2 to core

--- win/setup-core.ps1
+++ win/setup-core.ps1
@@ -1,2 +1,1 @@
-choco install -y vim git sysinternals winmerge notepadplusplus python wireshark gnu-units greenshot 7zip winscp putty microsoft-windows-terminal 
-
+choco install -y vim git sysinternals winmerge notepadplusplus python wireshark gnu-units 7zip winscp putty microsoft-windows-terminal vscode spotify docker-desktop wsl2 --params "/Retry:true"
